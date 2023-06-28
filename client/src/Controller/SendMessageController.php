2a6766497628d96d5254a65f2098f40cb6854fea
Max Atslega <max@atslega.dev>
2023-06-28 12:01:52 +0200

    Update SendMessageController to response with Json

--- client/src/Controller/SendMessageController.php
+++ client/src/Controller/SendMessageController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Message;
 use App\Form\MessageType;
 use App\Util\RpcClient;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class SendMessageController extends AbstractController {
 
     #[Route('/send', name: 'send_message', methods: ['POST'])]
-    public function send(Request $request): Response {
+    public function send(Request $request): JsonResponse {
         $message = new Message();
         $data = json_decode($request->getContent(), true);
 
@@ -26,9 +27,12 @@ class SendMessageController extends AbstractController {
             $rpcClient = new RpcClient();
             $response = $rpcClient->call($message->getMessage(), $message->getRoutingKey());
 
-            return new Response('Your response is: '. $response);
+            return new JsonResponse(['message' => 'Your response is: '. $response]);
         }
 
-        return new Response('Please provide a message in body', Response::HTTP_BAD_REQUEST);
+        return new JsonResponse([
+            'error' => 'Bad Request',
+            'message' => 'The request is invalid. Please provide both the \'message\' and \'routing_key\' in the request body.'
+        ], Response::HTTP_BAD_REQUEST);
     }
 }
