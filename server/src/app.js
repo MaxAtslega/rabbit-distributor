7999495514317bfc12544684c6245926d3911cac
Max Atslega <max@atslega.dev>
2023-06-28 13:14:28 +0200

    Update App to fetch data from the consumer and response to the queue

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,34 @@
-import { rabbitHost, rabbitPort, rabbitPassword, rabbitUser, rabbitExchange } from './config/index.js'
+import { rabbitHost, rabbitPort, rabbitPassword, rabbitUser, rabbitExchange, consumerAddress } from './config/index.js'
 import RpcServer from './utils/rpcServer.js'
+import axios from 'axios'
+import https from 'https'
 
+// Initialize AMQP connection
 const amqpConnection = new RpcServer(rabbitExchange)
 await amqpConnection.connect(rabbitHost, rabbitPort, rabbitUser, rabbitPassword)
 
-amqpConnection.channel.consume(amqpConnection.queue, function reply (msg) {
+// Consume messages from the queue
+amqpConnection.channel.consume(amqpConnection.queue, async function reply (msg) {
   const message = msg.content.toString()
-  console.log(' [.] [' + msg.fields.routingKey + '] ' + message)
+  const routingKey = msg.fields.routingKey
+  let response = 'error'
 
-  setTimeout(function () {
-    amqpConnection.channel.sendToQueue(msg.properties.replyTo,
-      Buffer.from(message.toString() + ' -> Antwort'), { correlationId: msg.properties.correlationId })
-    amqpConnection.channel.ack(msg)
-    console.log(' [x] [' + msg.fields.routingKey + '] Done')
-  }, 5 * 1000)
+  console.log(' [.] [Queue: ' + msg.fields.routingKey + '] [From: ' + msg.properties.replyTo + '] ' + message)
+
+  // Fetch data from the consumer
+  const httpsAgent = new https.Agent({ rejectUnauthorized: false })
+  await axios
+    .post(consumerAddress + '/' + routingKey, { message }, { httpsAgent })
+    .then((res) => { response = res.data?.message })
+    .catch((error) => {
+      console.error(error)
+    })
+
+  // Send the response to the queue
+  amqpConnection.channel.sendToQueue(msg.properties.replyTo,
+    Buffer.from(response), { correlationId: msg.properties.correlationId })
+  amqpConnection.channel.ack(msg)
+  console.log(' [x] Done')
 })
 
 export default amqpConnection
