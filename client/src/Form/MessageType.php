b1fa3013168392e7b9a56872b8f7af6496cf05ad
Max Atslega <max@atslega.dev>
2023-06-29 16:00:24 +0200

    Add docker files to client

--- client/src/Form/MessageType.php
+++ client/src/Form/MessageType.php
@@ -8,7 +8,10 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 final class MessageType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options): void {
-        $builder->add('message', TextType::class)->add('routing_key', TextType::class);
+        $builder
+            ->add('message', TextType::class)
+            ->add('routing_key', TextType::class)
+        ;
     }
 
 
